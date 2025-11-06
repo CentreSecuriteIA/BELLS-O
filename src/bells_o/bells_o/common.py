52cb86627d4235cd448aa714937f042a20c6c55e
Felix Michalak <felix@michalax.de>
2025-11-06 13:25:52 +0100

    Reduced complexity of common classes while keeping same functionality

--- src/bells_o/bells_o/common.py
+++ src/bells_o/bells_o/common.py
@@ -1,30 +1,32 @@
 """Define common datatypes and classes."""
 
-from typing import Any, Callable, NotRequired, Self, TypedDict, Unpack
+from collections.abc import Callable
+from typing import Any, Literal, NotRequired, Self, TypedDict, Unpack, get_args
 
 
-### Usage Type definitions
-USAGE_TYPES = ["JAILBREAK", "PROMPT_INJECTION", "CONTENT_MODERATION", "GENERAL"]
+### Usage Type definitions for type hinting, based on USAGE_TYPES defined above
+UsageType = Literal["jailbreak", "prompt_injection", "content_moderation"]
 
 
-class UsageKwargs(TypedDict):
-    """Type hinting class."""
+UsageTypes = TypedDict(
+    "UsageTypes",
+    {
+        "jailbreak": NotRequired[bool],
+        "prompt_injection": NotRequired[bool],
+        "content_moderation": NotRequired[bool],
+    },
+)
 
-    jailbreak: NotRequired[bool]
-    prompt_injection: NotRequired[bool]
-    content_moderation: NotRequired[bool]
 
+USAGE_TYPES = get_args(UsageType)
 
-# TODO: agree on usage types (e.g. General, jailbreak, etc.)
-
-# TODO: switch to collections.abc.callable instead of typing.callable
 # TODO: make boolean usage type parent class from which Usage and Result inherit (because they have multiple identical dunders)
 
 
-class Usage:
-    """Unifying class that acts as a usage type switch."""
+class Usage(dict):
+    """Class that represents the usage type of a dataset or supervisor."""
 
-    def __init__(self, *args: str) -> None:
+    def __init__(self, *args: UsageType) -> None:
         """Initialize a usage type object.
 
         All usage types that were listed during initialization will be set to true, the rest to False. All `<usage_type>` values can
@@ -35,79 +37,35 @@ class Usage:
             *args (str): Usage types that are supported
 
         """
-        lower_args = [arg.lower() for arg in args]
-        # if no types are passed, we assume it is a general type
-        if not args or "general" in lower_args:
-            self._usage_types: dict[str, bool] = {
-                usage_type.lower(): True for usage_type in USAGE_TYPES
-            }
+        # if no args are passed, we assume it is a general type
+        if not args:
+            kwargs = dict.fromkeys(USAGE_TYPES, True)
         else:
-            self._usage_types: dict[str, bool] = {
-                usage_type.lower(): False for usage_type in USAGE_TYPES
-            }
-            for arg in lower_args:
-                if arg.upper() not in USAGE_TYPES:
-                    print(f"WARNING: did not find {arg} in {USAGE_TYPES}, skipping.")
+            kwargs = {}
+            for arg in args:
+                if arg not in USAGE_TYPES:
+                    print(f"WARNING: '{arg}' is not in {USAGE_TYPES}, skipping '{arg}'.")
                     continue
-                self._usage_types[arg] = True
+                kwargs[arg] = True
+        super().__init__(**kwargs)
 
-    def values(self):
-        """Return a dict_values object of the usage types."""
-        return self._usage_types.values()
-
-    def keys(self):
-        """Return a list of the supported usage types."""
-        return [k for k, v in self._usage_types.items() if v]
-
-    def __iter__(self):
-        """Return iterator over supported usage types."""
-        return iter([k for k, v in self._usage_types.items() if v])
-
-    def __repr__(self):
-        return repr(self._usage_types)
-
-    def __getitem__(self, key: str) -> bool:
-        """Return the boolean value representing if a certain usage is part of this UsageType.
-
-        Args:
-            key (str): the name of the usage to be checked.
-
-        """
-        return self._usage_types[key]
-
-    def __setitem__(self, key: str, value: bool) -> None:
-        """Set the boolean value representing if a certain usage is part of this UsageType.
-
-        Args:
-            key (str): the name of the usage to be checked.
-            value (bool): the value.
-
-        """
-        self._usage_types[key] = value
-
-    def __eq__(self, other: Self) -> bool:
-        """Implement usage1 == usage2.
-
-        Currently fails if `other` is not of type `Usage`
-
-        Args:
-            other (Usage): Instance of `Usage` for which equivalence is checked.
-
-        """
-        return all(
-            self._usage_types[key.lower()] == other._usage_types[key.lower()] for key in USAGE_TYPES
-        )
-
-    def __ne__(self, other: Self):
-        """Inverse of Usage.__eg__."""
-        return not self.__eq__(other)
+    def __getitem__(self, key) -> bool:
+        """Return `True` if Usage supports this type, else `False`."""
+        if key in USAGE_TYPES:
+            try:
+                return super().__getitem__(key)
+            except KeyError:
+                return False
+        else:
+            raise ValueError(f"{key} if not a valid usage type. Expected one of {USAGE_TYPES}.")
 
 
 ### Typed dictionary definitions
 class Result(dict):
     """Unifying class that holds a result."""
 
-    def __init__(self, **kwargs: Unpack[UsageKwargs]):
+    def __init__(self, **kwargs: Unpack[UsageTypes]):
+        """Initialize Result object."""
         super().__init__(**kwargs)
 
     def __eq__(self, other: Self):
